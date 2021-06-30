7a88029d1cab3a366db5cabad24443b78e37b709
cmanouk <cmmanoukian@gmail.com>
2021-06-30 12:23:35 -0700

    init file structure for ct and backend

---
+++ your-favorite-lakers-backend/src/main/java/com/chasemanoukian/yourfavoritelakers/YourFavoriteLakersApplication.java
@@ -0,0 +1,13 @@
+package com.chasemanoukian.yourfavoritelakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourFavoriteLakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourFavoriteLakersApplication.class, args);
+	}
+
+}
---
+++ your-favorite-lakers-backend/src/test/java/com/chasemanoukian/yourfavoritelakers/YourFavoriteLakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chasemanoukian.yourfavoritelakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourFavoriteLakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
