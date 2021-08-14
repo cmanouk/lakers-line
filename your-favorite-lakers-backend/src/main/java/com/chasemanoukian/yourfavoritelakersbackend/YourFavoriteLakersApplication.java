48cb8b9c33c6c32afcccfae242c5ec86590b302b
cmanouk <cmmanoukian@gmail.com>
2021-08-14 14:36:28 -0700

    updated styles and altered Header component to fix minor bug

--- your-favorite-lakers-backend/src/main/java/com/chasemanoukian/yourfavoritelakersbackend/YourFavoriteLakersApplication.java
+++ your-favorite-lakers-backend/src/main/java/com/chasemanoukian/yourfavoritelakersbackend/YourFavoriteLakersApplication.java
@@ -20,4 +20,32 @@ public class YourFavoriteLakersApplication {
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
+//	@Bean
+//	public ServletWebServerFactory serverContainer() {
+//		//Enable SSL Traffic
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//			@Override
+//			protected void postProcessContext(Context context) {
+//				SecurityConstraint securityConstraint = new SecurityConstraint();
+//				securityConstraint.setUserConstraint("CONFIDENTIAL");
+//
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				securityConstraint.addCollection(collection);
+//				context.addConstraint(securityConstraint);
+//			}
+//		};
+//
+//		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+//		return tomcat;
+//	}
+//
+//	private Connector httpToHttpsRedirectConnector() {
+//		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//		connector.setScheme("http");
+//		connector.setPort(8080);
+//		connector.setSecure(false);
+//		connector.setRedirectPort(8443);
+//		return connector;
+//	}
 }
--- your-favorite-lakers-ct/client/components/Header/Header.js
+++ your-favorite-lakers-ct/client/components/Header/Header.js
@@ -3,10 +3,13 @@ import React from 'react';
 const Header = ({ setPlayerProfile, players, currPlayer }) => {
   return (
     <section className='header'>
-      <h1>
-        Lakers Line
-        <span>The best place to get up-to-date player stats</span>
-      </h1>
+      <div className="header__background"></div>
+      <div className="header__text">
+        <h1>
+          Lakers Line
+        </h1>
+        <p>The best place to get up-to-date player stats</p>
+      </div>
     </section>
   )
 }
